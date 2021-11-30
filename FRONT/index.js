211ebb5a5b4ccdf1a3b9156403d3fa05413c38dd
Vanessa-Helena <vanesa-lima10@hotmail.com>
2021-11-30 20:35:22 -0300

    home

--- FRONT/index.js
+++ FRONT/index.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const app = express();
+
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+//Model/View/Controller - MVC
+
+app.get('/', (req, res)=>{
+    res.render('home');
+});
+
+app.listen(3003, ()=>{
+    console.log("Servidor ativo na porta 3003");
+});

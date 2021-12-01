4cec9ec7c2cbc759dbfeef238db0d18752516f54
Vanessa-Helena <vanesa-lima10@hotmail.com>
2021-11-30 21:02:09 -0300

    styes

--- FRONT/index.js
+++ FRONT/index.js
@@ -3,14 +3,23 @@ const app = express();
 
 const path = require('path');
 
+app.use(express.static(path.join(__dirname,'public')));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 //Model/View/Controller - MVC
 
+
+
 app.get('/', (req, res)=>{
     res.render('home');
 });
+
+app.get('/login', (req, res)=>{
+    res.render('login.ejs');
+});
+
 
 app.listen(3003, ()=>{
     console.log("Servidor ativo na porta 3003");
